0a0537bf4c422d5ccd122cc3a7ccb1a66d1b5e49
Rex Bengil <centraleffects@yahoo.com>
2014-12-31 00:36:39 -0800

    Initial commit

---
+++ Skype.bat
@@ -0,0 +1,7 @@
+@echo off
+REM Created by Rex Bengil 
+REM centraleffects@yahoo.com
+ECHO Launching multi-skype program...
+start "" "C:\Program Files\Skype\Phone\Skype.exe" /secondary
+TIMEOUT 3
+exit /b
